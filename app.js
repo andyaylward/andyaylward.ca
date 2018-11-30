4ae99ad3f340635771a3fb9bfed8284f0add08e7
Andy Aylward <37965077+andyaylward@users.noreply.github.com>
2018-11-30 12:20:36 -0700

    Added more sections to main

--- app.js
+++ app.js
@@ -3,3 +3,18 @@ function pageLoadBodyEffect() {
     document.querySelector("body").style.transform = "translate(0,0)";
 }
 window.onload = pageLoadBodyEffect;
+
+const loadingBar = document.getElementById('loading-bar');
+
+function loadingBarProgress(){
+    for (i=0; i<101; i++){
+        loadingBar.style.width = (i + '%');
+    }  
+    // loadingBar.style.width = ('100%');
+    console.log(loadingBar.style.width);
+};
+
+setInterval(function() {
+    loadingBarProgress();
+});
+
